6a43f7db5a6cc1caa9426d4065bab0d46d8a7901
agarria <agarria@icloud.com>
2020-07-20 20:24:35 -0600

    Improvements

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+function menu() {
+    var menu = document.getElementById("menu");
+    var open = document.getElementById("menu_icon");
+    var close = document.getElementById("close_icon");
+    
+    menu.classList.toggle("show-menu");
+    open.classList.toggle("show-open");
+    close.classList.toggle("show-close");
+}
+
 $( document ).ready(function() {
 
 	$('.new-info').slick({
@@ -7,8 +17,31 @@ $( document ).ready(function() {
 		autoplay: true,
 		arrows: false,
 		dots: false,
-		pauseOnHover:false,
+		pauseOnHover: false,
 		autoplaySpeed: 2000
+	});
+
+	$('.new-info-mobile').slick({
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: true,
+		arrows: false,
+		dots: false,
+		pauseOnHover: false,
+		autoplaySpeed: 2000
+	});
+
+	$('.main-content').slick({
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: true,
+		arrows: false,
+		dots: true,
+		pauseOnHover: false,
+		autoplaySpeed: 4000,
+		vertical: true
 	});
 
 });
